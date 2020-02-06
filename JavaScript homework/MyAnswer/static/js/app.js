e0909e5e7de2c64ed0d3dc63cc4edffb69ddc2fc
charlesebrinkley <charlesebrinkley@comcast.net>
2020-02-06 13:41:29 -0500

    push on 2 6 2020

--- JavaScript homework/MyAnswer/static/js/app.js
+++ JavaScript homework/MyAnswer/static/js/app.js
@@ -1,16 +1,12 @@
 // from data.js
 var tableData = data;
 var tbody = d3.select("tbody");
-console.log(data);
-
-var ufoSight = data;
-var button = d3.select("#button");
+//console.log(data);
 
 
-// YOUR CODE HERE!
 // // Use d3 to update each cell's text with
 // // ufo report values (datetime, city, state, country, shape, durationMinutes, comments)
-data.forEach((ufoReport) => {
+tableData.forEach((ufoReport) => {
     var row = tbody.append("tr");
     Object.entries(ufoReport).forEach(([key, value]) => {
       var cell = row.append("td");
@@ -18,20 +14,39 @@ data.forEach((ufoReport) => {
     });
   });
 
+//code for querry based on date entered
 
-  button.on("click", function() {
+var ufoSight = data;
+var button = d3.select("button");
 
-    // Select the input element and get the raw HTML node
-    var inputElement = d3.select("#datetime");
+button.on("click", function() {
+  function removeTableBody(){
+    tbody.remove();
+  }
   
-    // Get the value property of the input element
-    var inputValue = inputElement.property("datetime");
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
   
-    console.log(inputValue);
-    console.log(ufoSight);
+
+  console.log(inputValue);
+  console.log(ufoSight);
   
-    var filteredData = ufoSight.filter(ufo => ufo.datetime === inputValue);
+  var filteredData = ufoSight.filter(ufo => ufo.datetime === inputValue);
   
-    console.log(filteredData);
-  
+  console.log(filteredData);
+
+
+ filteredData.forEach((ufoFilter) => {
+  var row = tbody.append("tr");
+  Object.entries(ufoFilter).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+    });
+
   });
+
+
+});
